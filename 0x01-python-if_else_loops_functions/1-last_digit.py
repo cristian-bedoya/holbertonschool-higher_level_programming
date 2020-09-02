6b5bfcc864e9a4fbb5e33409971dae9bee0e171b
cristian <crisbed321@gmail.com>
2020-09-02 00:13:18 +0000

    hot ifx

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,7 @@ if number >= 0:
     last_digit = number % 10
 else:
     last_digit = number % -10
-print("Last digit {:d} is {:d} and is".format(number, last_digit), end=' ')
+print('Last digit {:d} is {:d} and is'.format(number, last_digit), end=' ')
 if last_digit > 5:
     print('greater than 5')
 elif last_digit == 0:

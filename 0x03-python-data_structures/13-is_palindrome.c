5f03102eff29290796fac659848be113f573237e
cristian <crisbed321@gmail.com>
2020-09-09 21:21:58 +0000

    remove malloc 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -8,39 +8,27 @@
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *aux = *head, *aux_2 = *head;
-	int n_elements = 0;
+	listint_t *aux = *head;
 	int i = 0, index = 0;
-	int *arr;
+	int arr[2048];
 
 	if (!head)
 		return (0);
-	
-	for (n_elements = 0; aux; n_elements++)
-		aux = aux->next;
 
-	arr = malloc(sizeof(int) * (n_elements + 1));
-
-	if (!arr)
-		return (1);
-
-	for (index = 0; aux_2; index++)
+	for (index = 0; aux; index++)
 	{
-		arr[index] = aux_2->n;
-		aux_2 = aux_2->next;
+		arr[index] = aux->n;
+		aux = aux->next;
 	}
 
 	while (i < index / 2)
 	{
 		if (arr[i] != arr[index - 1])
 		{
-			free(arr);
 			return (0);
 		}
 		i++;
 		index--;
 	}
-	free(arr);
 	return (1);
-
 }

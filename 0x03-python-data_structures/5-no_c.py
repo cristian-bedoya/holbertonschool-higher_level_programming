46b6c5c960ffe9bf7a89c410a41a5ee54a9eeb73
cristian <crisbed321@gmail.com>
2020-09-09 00:00:46 +0000

    hot fix 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,15 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def no_c(my_string):
     if my_string:
         new_string = ''
-        for i in my_string:
-            if i not in "Cc":
-                new_string += i
-    return(new_string)
+        n_new = ''
+        for i in range(len(my_string)):
+            if my_string[i] == 'C' or my_string[i] == 'c':
+                new_string = my_string[:i] + my_string[i + 1:]
+                for j in range(len(new_string)):
+                    if new_string[j] == 'C' or new_string[j] == 'c':
+                        n_new = new_string[:j] + new_string[j + 1:]
+                        return n_new
+                    else:
+                        continue
+        return new_string

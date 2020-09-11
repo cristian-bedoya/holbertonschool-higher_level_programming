f5d990a8cc96a62a708220bb3716dacfbe28d42c
cristian <crisbed321@gmail.com>
2020-09-11 05:33:44 +0000

    hot fix 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,10 +4,10 @@ def roman_to_int(roman_string):
         return 0
     rom_d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     rom_n = 0
-    li = list(map(lambda x: rom_d.get(x), roman_string))
-    for i in range(len(li)):
-        if i > 0 and li[i] > li[i - 1]:
-            rom_n = li[i] - 2 * li[i - 1]
+    li_rom = li_romst(map(lambda x: rom_d.get(x), roman_string))
+    for i in range(len(li_rom)):
+        if i > 0 and li_rom[i] > li_rom[i - 1]:
+            rom_n += li_rom[i] - 2 * li_rom[i - 1]
         else:
-            rom_n += li[i]
+            rom_n += li_rom[i]
     return rom_n

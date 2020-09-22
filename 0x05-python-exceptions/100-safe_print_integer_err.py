4483c180d6da9c9b87429259e64d45d831205526
cristian <crisbed321@gmail.com>
2020-09-22 02:48:20 +0000

    advanced task 7 & 8

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        print('{:d}'.format(value))
+        return True
+    except Exception as err:
+        print("Exception:", err, file=sys.stderr)
+        return False

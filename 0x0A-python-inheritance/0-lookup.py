1bb31cd6d2f2d057e7b19f499c7b26b2267b23cf
cristian <crisbed321@gmail.com>
2020-10-07 02:29:36 +0000

    add docstring task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
+"""Module 0-lookup."""
+
+
 def lookup(obj):
+    """Returns that list of attributes and methods.
+    Args:
+        - obj: object to look into
+    """
+
     return dir(obj)

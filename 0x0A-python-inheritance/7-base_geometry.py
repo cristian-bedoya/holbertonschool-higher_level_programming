5fcc4f6eaecc981680c52ab9d6b6194d53538772
cristian <crisbed321@gmail.com>
2020-10-07 03:32:49 +0000

    quit self.

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
-"""Module 6-base_geometry."""
+"""Module 7-base_geometry.
+"""
 
 
 class BaseGeometry:
-    """Class with public instance method."""
+    """Class with public instance methods."""
 
     def area(self):
         """Raises an Exception with the message
         'area() is not implemented'.
         """
+
         raise Exception('area() is not implemented')
 
     def integer_validator(self, name, value):
         """Validates value."""
+
         if type(value) is not int:
-            raise TypeError('{} must be an integer'.format(self.name))
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError('{} must be greater than 0'.format(self.name))
+            raise ValueError("{} must be greater than 0".format(name))

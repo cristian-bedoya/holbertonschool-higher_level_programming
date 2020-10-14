0a446113bc5dc992b1a04ce40a158bb650b0cc7b
cristian <crisbed321@gmail.com>
2020-10-14 04:21:04 +0000

    add csv serializer and  dese

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -67,3 +67,27 @@ class Base():
             str1 = f.read()
             list_dict = cls.from_json_string(str1)
         return [cls.create(**d) for d in list_dict]
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """Writes the CSV string representation of
+        list_objs to a file."""
+        if list_objs is None or list_objs == []:
+            c_str = "[]"
+        else:
+            c_str = cls.to_json_string([o.to_dictionary() for o in list_objs])
+        new_file = cls.__name__ + ".csv"
+        with open(new_file, 'w') as f:
+            f.write(c_str)
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """Returns an instance with all attributes already set."""
+        file_1 = cls.__name__ + '.csv'
+        if not os.path.exists(file_1):
+            return []
+        list_dict = []
+        with open(file_1, 'r') as f:
+            str1 = f.read()
+            list_dict = cls.from_json_string(str1)
+        return [cls.create(**d) for d in list_dict]

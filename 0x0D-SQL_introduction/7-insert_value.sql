7ae32d05a4931e624de6b442e5a84b83b0ac75e0
cristian-bedoya <crisbed321@gmail.com>
2020-11-17 03:56:39 +0000

    add task in project Sql  0-9

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,4 @@
+-- Write a script that inserts a new row in the table first_table (database hbtn_0c_0).
+-- New row: id = 89, name = Holberton School
+INSERT INTO first_table (id, name) VALUE (89, "Holberton School");
+ 

e2d0e0ea049ac998a3b263a2ad8bddbbd71b98e4
cristian-bedoya <crisbed321@gmail.com>
2021-02-03 04:53:46 +0000

    addd task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const argPass = process.argv[2];
+if (process.argv.length === 2) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < argPass; i++) {
+    let y = '';
+    for (let j = 0; j < argPass; j++) {
+      y += 'X';
+    }
+    console.log(y);
+  }
+}

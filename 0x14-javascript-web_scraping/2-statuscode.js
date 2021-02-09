73c35d11d11ea866310e6edba21a31e91529d965
cristian-bedoya <crisbed321@gmail.com>
2021-02-09 18:12:28 +0000

    add task 2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const request = require('request');
+request(process.argv[2], function (error, response) {
+  if (error) {
+    console.error('error:', error);
+  }
+  console.log('code:', response.statusCode); // Print the response status code if a response was received
+});
